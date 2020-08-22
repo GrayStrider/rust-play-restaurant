ddff3a5ee744cf3dc225399cf90dbaf6e50e5114
Gray Strider <zhoga.ivan@gmail.com>
2020-08-22 14:14:38 +1100

    add crate examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,83 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		assert_eq!(2 + 2, 4);
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+mod front_of_house {
+	pub(crate) mod hosting {
+		pub(crate) fn add_to_waitlist() {}
+		
+		fn seat_at_table() {}
+	}
+	
+	mod serving {
+		fn take_order() {}
+		
+		fn serve_order() {}
+		
+		fn take_payment() {}
 	}
 }
+
+// Bringing the function’s parent module into scope
+// with use so we have to specify the parent module
+// when calling the function makes it clear that the
+// function isn’t locally defined while still minimizing repetition of the full path
+use crate::front_of_house::hosting;
+use self::front_of_house::hosting::add_to_waitlist; // not ideomatic for functions
+
+pub fn eat_at_restaurant() {
+	// Absolute path
+	crate::front_of_house::hosting::add_to_waitlist();
+	
+	// Relative path
+	front_of_house::hosting::add_to_waitlist();
+	
+	// or put the whole thing in scope
+	hosting::add_to_waitlist();
+	hosting::add_to_waitlist();
+	hosting::add_to_waitlist();
+	
+	// Order a breakfast in the summer with Rye toast
+	let mut meal = back_of_house::Breakfast::summer("Rye");
+	// Change our mind about what bread we'd like
+	meal.toast = String::from("Wheat");
+	println!("I'd like {} toast please", meal.toast);
+	
+	// The next line won't compile if we uncomment it; we're not allowed
+	// to see or modify the seasonal fruit that comes with the meal
+	// meal.seasonal_fruit = String::from("blueberries");
+	
+	let order1 = back_of_house::Appetizer::Soup;
+	let order2 = back_of_house::Appetizer::Salad;
+	
+}
+
+fn serve_order() {}
+
+mod back_of_house {
+	fn fix_incorrect_order() {
+		cook_order();
+		super::serve_order();
+	}
+	
+	fn cook_order() {}
+	
+	pub struct Breakfast {
+		pub toast: String,
+		seasonal_fruit: String,
+	}
+	
+	pub enum Appetizer {
+		Soup,
+		Salad,
+	}
+	
+	impl Breakfast {
+		pub fn summer(toast: &str) -> Breakfast {
+			Breakfast {
+				toast: String::from(toast),
+				seasonal_fruit: String::from("peaches"),
+			}
+		}
+	}
+}
+
